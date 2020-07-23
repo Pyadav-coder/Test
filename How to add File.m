eb07bc8bd36754fea83a213d6304bc4889c132ad
admin <admin@DESKTOP-HASP0AN>
2020-07-23 19:13:02 +0530

    added steps to add file fom "local repository" to "remote repository".

---
+++ How to add File.m
@@ -0,0 +1,8 @@
+%We are adding file 'How to add File.m' from our "local repository" to "remote repository".
+
+%Steps%
+
+%1.Right click and add new file to local repository.
+%2.Right click on the newly added file go to-->source control --> add to git.
+%3.After doing some changes in your newly added file, you can view and
+%commit those changes by going to--> source control --> view and commit changes
