3fee9206e8768f5f4b5d3c4cbede2d3c7de5a701
admin <admin@DESKTOP-HASP0AN>
2020-07-23 19:20:20 +0530

    added steps of uploading a file from "local repository" to "remote repository".

--- How to add File.m
+++ How to add File.m
@@ -3,6 +3,10 @@
 %Steps%
 
 %1.Right click and add new file to local repository.
-%2.Right click on the newly added file go to-->source control --> add to git.
+%2.Right click on the newly added file go to--> "source control"--> "add to git".
 %3.After doing some changes in your newly added file, you can view and
-%commit those changes by going to--> source control --> view and commit changes
+%commit those changes by going to-->"source control"--> "view and commit
+%changes"
+%4.At last "push" the file after commiting changes in order to upload it on
+%remote repository.Enter credentials of your github account and press
+%"commit"
